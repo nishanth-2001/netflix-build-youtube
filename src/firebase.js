dc591905cc2a5329833db806cb56e1de2e877448
Nishanth <nishanthanand2001@gmail.com>
2024-01-18 11:45:47 +0530

    netflix clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBufniyYehPDkgwikkdc-AGAFqpqRul5Ig",
+  authDomain: "netflix-clone-9fd68.firebaseapp.com",
+  projectId: "netflix-clone-9fd68",
+  storageBucket: "netflix-clone-9fd68.appspot.com",
+  messagingSenderId: "867181084628",
+  appId: "1:867181084628:web:d442c5cd97f28556046041",
+};
